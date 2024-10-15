2c96fb1d4ed526b1ad0d22dc1888e1e6416c5c40
sabani <sabanibjm123@gmail.com>
2024-10-15 15:45:34 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_application_pertama/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_application_pertama
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ bin/dart daasar/symbol.dart
@@ -0,0 +1,9 @@
+void main() {
+Symbol symbol1 = Symbol('Muhammad syabani');
+var symbol2 = #belajar;
+
+print(symbol1);
+print(symbol2);
+
+
+}
